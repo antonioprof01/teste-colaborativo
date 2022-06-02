49ad0059eac4d66b70749d8a05a73d8cff385d6b
WILLDEGLAN GOMES DA SILVA <00257309322@ifto.local>
2022-06-01 21:55:27 -0300

    update app

--- colaborativo/src/App.java
+++ colaborativo/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Aqui sera criado um projeto teste colaborativo");
+        System.out.println("Aqui sera criado um projeto teste colaborativo\n\n");
+
+        Endereco end = new Endereco();
+        Empresa emp = new Empresa();
+        Funcionario fun = new Funcionario("will", "hilp25@gmail.com", 845555, "RH");
+
+        end.setCidade("colinas");
+        end.setEstado("TO");
+        end.setNumero(548);
+        end.setRua("Rua Goianésia");
+
+        emp.setCidade("Nova Olinda");
+        emp.setTelefone(845698754);
+        emp.setEmail("hilp25@hotmail.com");
+
+        System.out.println(fun.toString());
+        System.out.println(end.toString());
+        System.out.println(emp.toString());
     }
 }
